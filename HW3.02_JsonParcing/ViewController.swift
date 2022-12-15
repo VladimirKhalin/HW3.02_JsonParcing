fc82af064985e470128f7fe9f9a2bba413cb5bc7
VK <art.nebar@mail.ru>
2022-12-15 18:06:01 +0300

    Initial Commit

---
+++ HW3.02_JsonParcing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW3.02_JsonParcing
+//
+//  Created by Vladimir Khalin on 15.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
